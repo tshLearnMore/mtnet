83718944b3cab2b969665ab13cac427bd7606a2b
TSH <tsh_tian@163.com>
2019-03-10 20:54:09 +0800

    mtnet:更新测试程序test1

--- mtnet/mtnetTest/test1.cc
+++ mtnet/mtnetTest/test1.cc
@@ -1,17 +1,18 @@
 #include "EventLoop.h"
+#include "Thread.h"
 #include <stdio.h>
-
+#include <unistd.h>
 
 mtnet::EventLoop* g_loop;
 
 void print()
 {
-	printf("exc runAfter(1.0,print) \n");
+	printf("exc runAfter(1.0,print) tid = %d\n",mtnet::CurrentThread::tid());
 };
 
 void printInterval()
 {
-	printf("exc runEvery(2.0,printInterval) \n");
+	printf("exc runEvery(2.0,printInterval) tid = %d\n",mtnet::CurrentThread::tid());
 }
 
 void threadFunc()
@@ -25,8 +26,11 @@ int main()
 {
 	mtnet::EventLoop loop;
 	g_loop = &loop;
-	mtnet::Thread t(threadFunc);
-	t.start();
+	mtnet::Thread t1(threadFunc);
+	t1.start();
+	mtnet::Thread t2(threadFunc);
+	t2.start();
+	sleep(2);
 	loop.loop();
 	return 0;
 }
